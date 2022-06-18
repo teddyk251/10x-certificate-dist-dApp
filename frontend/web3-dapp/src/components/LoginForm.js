afa7b470aeffe11daa43bb07e7dd765a85c72af6
Teddy <teddylnk1@gmail.com>
2022-06-18 22:39:35 +0300

    update endpoint

--- frontend/web3-dapp/src/components/LoginForm.js
+++ frontend/web3-dapp/src/components/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = () => {
     const submitHandler = (e) => {
         e.preventDefault();
 
-        fetch('http://127.0.0.1:5000/login', {
+        fetch('https://web3-flask-backend.herokuapp.com/login', {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(details)
