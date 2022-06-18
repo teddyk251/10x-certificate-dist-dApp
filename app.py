031bab14ae742eef56c01668642cde4073cc1245
Teddy <teddylnk1@gmail.com>
2022-06-18 14:15:29 +0300

    implement login api

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, json, request
+import pymongo
 
 app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://teddyk:JToi5MPMTkwg6G9L@cluster0.zka8x.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database('webapp')
+users = db.users
 
 app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    if users.find_one({'email': email, 'password': password}):
+        return json.dumps({'success': True})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
