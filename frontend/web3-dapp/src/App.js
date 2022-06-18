4a8d71d4386f776cdb0fb83892f36e4649697d68
Teddy <teddylnk1@gmail.com>
2022-06-18 10:55:23 +0300

    implement loginform

--- frontend/web3-dapp/src/App.js
+++ frontend/web3-dapp/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import React from 'react';
+import LoginForm from './components/LoginForm';
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import SignUpForm from './components/SignupForm';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        {/* <Switch> */}
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/signup" component={SignUpForm} />
+        {/* </Switch> */}
+      </div>
+    </Router>
   );
 }
 
