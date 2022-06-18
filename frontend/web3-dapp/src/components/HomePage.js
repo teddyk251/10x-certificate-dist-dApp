03a0c665f520b54723827d81d5ad8e2c62db7a63
Teddy <teddylnk1@gmail.com>
2022-06-18 12:17:58 +0300

    refactor homepage

--- frontend/web3-dapp/src/components/HomePage.js
+++ frontend/web3-dapp/src/components/HomePage.js
@@ -36,12 +36,6 @@ function HomePage() {
 
   return (
       <div className="App">
-        {/* {(user.email !== "") ? (
-          <div className="welcome">
-            <h2>Welcome, <span>{user.name}</span></h2>
-            <button onClick={Logout}>Logout</button>
-          </div>
-        ) : ( */}
           <LoginForm Login={Login} error={error} />
           <Link to="/signup"> Sign Up </Link>
         
