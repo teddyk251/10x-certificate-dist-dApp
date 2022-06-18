888ac7064a02e6381c42984f62343c8ae94a341b
Teddy <teddylnk1@gmail.com>
2022-06-18 10:55:46 +0300

    implement navbar

---
+++ frontend/web3-dapp/src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+
+const Navbar = ({user}) => {
+    return ( 
+       <nav className="navbar">
+        <h1>10 Academy Certificate Issuer</h1>
+        <div className="links">
+            {/* <Link to="/signup">SignUp</Link> */}
+            {/* <Link to="/create">New Blog</Link> */}
+        </div>
+       </nav> 
+     );
+}
+ 
+export default Navbar;
